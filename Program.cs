c17a53476f863ffd24442ae3a6653a7f69c41d9b
Antonio Marco Furio <ICTS21-23.406@studenti.local>
2022-04-12 14:46:34 +0200

    filesss

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProgettoGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public string Antonio()
+            {
+                Console.WriteLine("Il mio nome è Antonio");
+            }
+        }
+    }
+}
